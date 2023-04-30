bee4f8a287f76eea79ed3babceaaa661ca7ff408
Граница Дмитрий <granicadmitrij@MacBook-Air-Granica.local>
2023-04-30 15:40:09 +0400

    First part of book "Refactoring"

---
+++ src/part/first/java/org/example/Rental.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import lombok.Getter;
+
+@Getter
+public class Rental {
+    private Movie movie;
+    private int daysRented;
+
+    public Rental(Movie movie, int daysRented) {
+        this.movie = movie;
+        this.daysRented = daysRented;
+    }
+}
