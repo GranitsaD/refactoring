762f6d93ada452b53b7d4dc34dd3259ce2368fc2
granicadmitrij <dimagranitsa@mail.ru>
2023-04-30 15:52:41 +0400

    Replace Temp with Query

--- src/part/first/java/org/example/Customer.java
+++ src/part/first/java/org/example/Customer.java
@@ -25,33 +25,17 @@ public class Customer {
         Enumeration rentals = _rentals.elements();
         String result = "Учет аренды для " + getName() + "\n";
         while (rentals.hasMoreElements()) {
-            double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
-            //определить сумму для каждой строки
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDREN:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    break;
-            }
-            // добавить очки для активного арендатора
+
             frequentRenterPoints++;
+
             // бонус за аренду новинки на два дня
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
                     each.getDaysRented() > 1) frequentRenterPoints++;
+
             //показать результаты для этой аренды
-            result += "\t" + each.getMovie().getTitle() + "\t" +
-                    String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+            result += "\t" + each.getMovie().getTitle() + "\t" + each.getCharge() + "\n";
+            totalAmount += each.getCharge();
         }
 
         //добавить нижний колонтитул
@@ -61,4 +45,5 @@ public class Customer {
                 " очков за активность";
         return result;
     }
+
 }
